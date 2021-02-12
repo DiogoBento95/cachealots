26c78d182835db8a51c2070db068f90a6ac0d3ed
paulo <pauloblemos@gmail.com>
2021-02-12 22:41:50 -0100

    add the third game

---
+++ Game3/src/org/terceira/academiadecodigo/bootcamp/cachaloteinvader/ConfirmBite.java
@@ -0,0 +1,124 @@
+package org.terceira.academiadecodigo.bootcamp.cachaloteinvader;
+
+import org.academiadecodigo.simplegraphics.graphics.Canvas;
+import org.academiadecodigo.simplegraphics.graphics.Shape;
+import org.academiadecodigo.simplegraphics.graphics.Text;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+import java.util.List;
+
+public class ConfirmBite {
+
+    private boolean squidBitten = false;
+
+    private boolean cachaloteBitten = false;
+
+    private Picture cachalote;
+    private Picture[] shark;
+    private Picture squid;
+
+    private Text counter;
+
+    private int squidsEaten = 0;
+
+    public ConfirmBite(Picture cachalote, Picture[] shark, Picture squid) {
+
+        this.cachalote = cachalote;
+        this.shark = shark;
+        this.squid = squid;
+        this.counter = new Text(10, 10, "Squids eaten: " + squidsEaten);
+        counter.draw();
+    }
+
+    public void bite(int j) {
+            if(cachaloteBitten == true) {
+                return;
+            }
+            if ((cachalote.getMaxX() >= shark[j].getX() && cachalote.getMaxX() <= shark[j].getMaxX()) &&
+                    ( cachalote.getMaxY() >= shark[j].getY() && cachalote.getMaxY() <= shark[j].getMaxY())
+                    ||
+                    (cachalote.getMaxX() >= shark[j].getX() && cachalote.getMaxX() <= shark[j].getMaxX()) &&
+                           ( cachalote.getY() >= shark[j].getY() && cachalote.getY() <= shark[j].getMaxY())
+||
+        ((shark[j].getMaxX() >= cachalote.getX() && shark[j].getMaxX() <= cachalote.getMaxX()) &&
+                ( shark[j].getMaxY() >= cachalote.getY() && shark[j].getMaxY() <= cachalote.getMaxY())
+                ||
+                (shark[j].getMaxX() >= cachalote.getX() && shark[j].getMaxX() <= cachalote.getMaxX()) &&
+                        ( shark[j].getY() >= cachalote.getY() && shark[j].getY() <= cachalote.getMaxY())))
+
+
+            {
+
+                List<Shape> shapes = Canvas.getInstance().getShapes();
+                shapes.clear();
+                Picture ocean = new Picture(10,10,"resources/ocean.gif");
+                ocean.draw();
+                Picture gameOver = new Picture(100,100,"resources/win.png");
+                gameOver.draw();
+
+                setBiteFalse(true);
+
+            }
+
+    }
+
+    public void biteSquid() {
+
+        if ((cachalote.getMaxX() >= squid.getX() && cachalote.getMaxX() <= squid.getMaxX()) &&
+                ( cachalote.getMaxY() >= squid.getY() && cachalote.getMaxY() <= squid.getMaxY())
+                ||
+                (cachalote.getMaxX() >= squid.getX() && cachalote.getMaxX() <= squid.getMaxX()) &&
+                        ( cachalote.getY() >= squid.getY() && cachalote.getY() <= squid.getMaxY())
+                ||
+                ((squid.getMaxX() >= cachalote.getX() && squid.getMaxX() <= cachalote.getMaxX()) &&
+                        ( squid.getMaxY() >= cachalote.getY() && squid.getMaxY() <= cachalote.getMaxY())
+                        ||
+                        (squid.getMaxX() >= cachalote.getX() && squid.getMaxX() <= cachalote.getMaxX()) &&
+                                ( squid.getY() >= cachalote.getY() && squid.getY() <= cachalote.getMaxY())))
+
+
+        {
+            counter.delete();
+            squid.delete();
+            newSquid();
+            squidsEaten++;
+            counter.setText("Squids eaten: " + squidsEaten);
+            counter.draw();
+            System.out.println(squidsEaten);
+
+        }
+
+        return;
+
+    }
+
+    public void newSquid() {
+
+        int randomNumberX = (int) Math.round(Math.random() * 900);
+        int randomNumberY = (int) Math.round(Math.random() * 500);
+
+        squid = new Picture(randomNumberX,randomNumberY,"resources/squid.png");
+        squid.draw();
+
+        biteSquid();
+
+        return;
+
+    }
+
+    public void setBiteFalse(boolean bitten) {
+        cachaloteBitten = bitten;
+    }
+
+    public boolean isCachaloteBitten() {
+        return cachaloteBitten;
+    }
+
+    public boolean isSquidBitten() {
+        return squidBitten;
+    }
+
+    public int getSquidsEaten() {
+        return squidsEaten;
+    }
+}
---
+++ Game3/src/org/terceira/academiadecodigo/bootcamp/cachaloteinvader/Game3.java
@@ -0,0 +1,134 @@
+package org.terceira.academiadecodigo.bootcamp.cachaloteinvader;
+
+import org.academiadecodigo.simplegraphics.graphics.Canvas;
+import org.academiadecodigo.simplegraphics.graphics.Shape;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+import java.util.List;
+
+public class Game3 {
+
+    public static final int PADDING = 10;
+
+    public Picture cachaloteLeft;
+    public Picture cachaloteRight;
+    public Picture squid;
+
+    private ConfirmBite confirmBite;
+
+    private Picture shark1;
+    private Picture shark2;
+    private Picture shark3;
+    private Picture shark4;
+    private Picture shark5;
+    private Picture shark6;
+
+// Starts the game, instantiates the background, whale, keyboard eventlisteners and initializes the enemy method
+public void init() throws InterruptedException {
+
+    Picture sea = new Picture(PADDING,PADDING,"resources/ocean.gif");
+    sea.draw();
+
+    cachaloteLeft = new Picture(450,250, "resources/WhaleL.png");
+    cachaloteRight = new Picture(450,250, "resources/whaleR.png");
+    cachaloteLeft.delete();
+    cachaloteRight.draw();
+
+    squid = new Picture(200,200,"resources/squid.png");
+    squid.draw();
+
+    WhaleKeyboardHandler whaleHandler = new WhaleKeyboardHandler(cachaloteLeft, cachaloteRight);
+    Keyboard whaleKey = new Keyboard(whaleHandler);
+
+    whaleKey.addEventListener(KeyboardEvent.KEY_RIGHT, KeyboardEventType.KEY_RELEASED);
+    whaleKey.addEventListener(KeyboardEvent.KEY_LEFT, KeyboardEventType.KEY_RELEASED);
+    whaleKey.addEventListener(KeyboardEvent.KEY_UP, KeyboardEventType.KEY_RELEASED);
+    whaleKey.addEventListener(KeyboardEvent.KEY_DOWN, KeyboardEventType.KEY_RELEASED);
+
+    enemy();
+
+    whaleHandler.setKeyboardEndGameTrue();
+
+    System.out.println("DELETE ME");
+
+    return;
+
+}
+
+// Instantiates sharks and moves them against the wale
+public void enemy() throws InterruptedException {
+
+    Picture[] arraySharks = new Picture[6];
+
+    arraySharks[0] = shark1;
+    arraySharks[1] = shark2;
+    arraySharks[2] = shark3;
+    arraySharks[3] = shark4;
+    arraySharks[4] = shark5;
+    arraySharks[5] = shark6;
+
+    confirmBite = new ConfirmBite(cachaloteLeft, arraySharks, squid);
+
+        while(!confirmBite.isCachaloteBitten()) {
+
+            for (int i = 0; i < 3; i++) {
+                int randomY = (int) Math.round(Math.random() * 500);
+                arraySharks[i] = new Picture(869,randomY,"resources/SharkL.png");
+                arraySharks[i].draw();
+            }
+
+            for (int i = 3; i < arraySharks.length; i++) {
+                int randomY = (int) Math.round(Math.random() * 500);
+                arraySharks[i] = new Picture(PADDING,randomY,"resources/SharkR.png");
+                arraySharks[i].draw();
+            }
+
+            while(arraySharks[0].getX() > PADDING  && !confirmBite.isSquidBitten()) {
+
+                    for (int i = 0; i < arraySharks.length; i++) {
+
+                        confirmBite.biteSquid();
+
+                        if(confirmBite.getSquidsEaten() > 2) {
+
+                            List<Shape> shapes = Canvas.getInstance().getShapes();
+                            shapes.clear();
+
+                            Picture sea = new Picture(PADDING,PADDING,"resources/ocean.gif");
+                            sea.draw();
+
+                            Picture youWin = new Picture(500,250,"resources/win.png");
+
+                            youWin.draw();
+
+                            return;
+
+                        }
+                        if (i < 3) {
+                            arraySharks[i].translate(-10, 0);
+                            confirmBite.bite(i);
+                            Thread.sleep(20);
+                            continue;
+                        }
+                        arraySharks[i].translate(10, 0);
+                        confirmBite.bite(i);
+                        Thread.sleep(20);
+
+                        if(confirmBite.isCachaloteBitten() == true) {return; }
+
+                    }
+            }
+            System.out.println("DELETE ME FIRST");
+        }
+    System.out.println("DELETE ME AGAIN");
+
+        return;
+
+    }
+
+    }
+
+
